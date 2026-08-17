983c7d59c1e61aa58397e61352897dcfd3eec13b
Nivedh E R <nivedher12@gmail.com>
2026-08-17 19:25:34 +0530

    feat(signUpValidator): added signup validator

---
+++ backend/src/validators/sign-up.validator.js
@@ -0,0 +1,36 @@
+import { body } from "express-validator";
+import { prisma } from "../lib/prisma.js";
+
+export const signUpValidation = [
+  body("username")
+    .trim()
+    .notEmpty()
+    .withMessage("Username is required")
+    .isLength({ min: 3 })
+    .withMessage("Username must be atleast 3 characters long")
+    .escape(),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    .custom(async (value) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          email: value,
+        },
+      });
+      if (user) {
+        throw new Error("User with thie email already exists");
+      }
+    })
+    .normalizeEmail(),
+  body("password")
+    .isLength({ min: 5 })
+    .withMessage("Password must be atleast 5 characters long"),
+  body("confirmPassword").custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error("The passwords don't match");
+    }
+    return true;
+  }),
+];
