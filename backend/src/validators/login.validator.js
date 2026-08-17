0d09c7acab45c297f092ef6b637a6653ca8c4c61
Nivedh E R <nivedher12@gmail.com>
2026-08-17 19:25:22 +0530

    feat(loginValidator): added login validator

---
+++ backend/src/validators/login.validator.js
@@ -0,0 +1,30 @@
+import { body } from "express-validator";
+import { prisma } from "../lib/prisma.js";
+import bcrypt from "bcrypt";
+
+export const loginValidation = [
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("Please provide a valid email")
+    .custom(async (value) => {
+      const user = await prisma.user.findUnique({
+        where: { email: value },
+      });
+      if (!user) {
+        throw new Error("Invalid email or password");
+      }
+    })
+    .normalizeEmail(),
+  body("password").custom(async (value, { req }) => {
+    const user = await prisma.user.findUnique({
+      where: { email: req.body.email },
+    });
+    if (user) {
+      const password = await bcrypt.compare(value, user.password);
+      if (!password) {
+        throw new Error("Invalid email or password");
+      }
+    }
+  }),
+];
