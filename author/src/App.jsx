45c8e37474df541fe2278f2d644df9b15b4afe25
Nivedh E R <nivedher12@gmail.com>
2026-08-18 18:20:29 +0530

    feat(LoadingSpinner): added a loading spinner

--- author/src/App.jsx
+++ author/src/App.jsx
@@ -1,13 +1,26 @@
 import "./styles/App.css";
-import Home from "./pages/Home";
+import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
+import { useAuth } from "./hooks/useAuth";
+import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
+  const { loading } = useAuth();
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <div className="app__main-container">
+      {loading ? (
+        <div className="app__loading-container">
+          <LoadingSpinner loading={loading} />
+        </div>
+      ) : (
+        <div className="app__content-container">
+          <Navbar />
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      )}
+    </div>
   );
 }
 
