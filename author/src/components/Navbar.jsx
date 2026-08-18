aaaadee1be504715158cf38479da82be96ea1c4e
Nivedh E R <nivedher12@gmail.com>
2026-08-18 22:33:46 +0530

    feat(lucide-react): added lucide icons

--- author/src/components/Navbar.jsx
+++ author/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import "../styles/Navbar.css";
 import { useAuth } from "../hooks/useAuth";
+import { LogIn, LogOut, User } from "lucide-react";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -11,18 +12,23 @@ const Navbar = () => {
       </Link>
       {user ? (
         <div className="navbar__name-container">
-          <p>@{user.username}</p>
+          <div className="navbar__user-container">
+            <User size={20} />
+            <p>{user.username}</p>
+          </div>
           <button
             className="navbar__logout-btn"
             onClick={logout}
             title="Logout"
           >
             Logout
+            <LogOut size={20} />
           </button>
         </div>
       ) : (
         <div className="navbar__link-container">
           <Link className="navbar__login-btn" to="/auth/login" title="Login">
+            <LogIn size={20} />
             Login
           </Link>
           <Link
