69632e9e00508c76cd540f5dc2cd1d0d08387967
Nivedh E R <nivedher12@gmail.com>
2026-08-18 18:20:48 +0530

    refactor(content): removed the unnecessary content from Home

--- author/src/pages/Home.jsx
+++ author/src/pages/Home.jsx
@@ -1,16 +1,3 @@
-import { useEffect, useState } from "react";
-
-const Home = () => {
-  const [data, setData] = useState("");
-  useEffect(() => {
-    fetch("http://localhost:3000")
-      .then((res) => res.json())
-      .then((data) => {
-        setData(data.message);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-  return <h1>{data}</h1>;
-};
+const Home = () => {};
 
 export default Home;
