650005d6d971163a57cfb8e21c8657699e38c00c
Nivedh E R <nivedher12@gmail.com>
2026-08-19 19:17:56 +0530

    feat(alert-dialog): added an alert dialog on logging out

--- author/src/components/Navbar.jsx
+++ author/src/components/Navbar.jsx
@@ -2,46 +2,84 @@ import { Link } from "react-router";
 import { useAuth } from "../hooks/useAuth";
 import { LogOut, User } from "lucide-react";
 import { Button } from "./ui/button";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
   return (
-    <div className="flex justify-between items-center px-4 py-3 border-b border-gray-500">
-      <Link to="/" className="text-2xl font-bold">
-        Blogo
-      </Link>
-      {user ? (
-        <div className="flex gap-4 items-center">
-          <div className="flex gap-2">
-            <User size={20} />
-            <p>{user.username}</p>
+    <div className="">
+      <div className="flex justify-between items-center px-4 py-3 border-b border-gray-500">
+        <Link to="/" className="text-2xl font-bold">
+          Blogo
+        </Link>
+        {user ? (
+          <div className="flex gap-4 items-center">
+            <div className="flex gap-2">
+              <User size={20} />
+              <p>{user.username}</p>
+            </div>
+            <AlertDialog>
+              <AlertDialogTrigger
+                render={
+                  <Button
+                    className="flex gap-2 cursor-pointer"
+                    title="Logout"
+                    variant="destructive"
+                  >
+                    Logout
+                    <LogOut size={20} />
+                  </Button>
+                }
+              />
+              <AlertDialogContent className={"dark"}>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    Are you sure you want to logout?
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel className={"cursor-pointer"}>
+                    Cancel
+                  </AlertDialogCancel>
+                  <AlertDialogAction
+                    className={"cursor-pointer"}
+                    onClick={logout}
+                    variant="destructive"
+                  >
+                    Logout
+                  </AlertDialogAction>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </div>
-          <Button
-            className="flex gap-2 cursor-pointer"
-            onClick={logout}
-            title="Logout"
-            variant="destructive"
-          >
-            Logout
-            <LogOut size={20} />
-          </Button>
-        </div>
-      ) : (
-        <div className="flex items-center gap-2">
-          <Link
-            className="flex items-center gap-1 cursor-pointer"
-            to="/auth/login"
-            title="Login"
-          >
-            <Button variant="ghost" className="cursor-pointer">
-              Login
-            </Button>
-          </Link>
-          <Link className="cursor-pointer" to="/auth/sign-up" title="Sign Up">
-            <Button className="cursor-pointer">Sign Up</Button>
-          </Link>
-        </div>
-      )}
+        ) : (
+          <div className="flex items-center gap-2">
+            <Link
+              className="flex items-center gap-1 cursor-pointer"
+              to="/auth/login"
+              title="Login"
+            >
+              <Button variant="ghost" className="cursor-pointer">
+                Login
+              </Button>
+            </Link>
+            <Link className="cursor-pointer" to="/auth/sign-up" title="Sign Up">
+              <Button className="cursor-pointer">Sign Up</Button>
+            </Link>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
