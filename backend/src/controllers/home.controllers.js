f787c6bc5e34c0c659b87eb8cd4d2151aafc3419
Nivedh E R <nivedher12@gmail.com>
2026-08-20 20:50:45 +0530

    feat(ensureAuth and dummy data): added ensureAuth function and added dummy json to homeGetController

---
+++ backend/src/controllers/home.controllers.js
@@ -0,0 +1,5 @@
+export const homeGetController = (req, res, next) => {
+  res.json({
+    message: "hey from home",
+  });
+};
--- backend/src/routes/home.routes.js
+++ backend/src/routes/home.routes.js
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { ensureAuth } from "../middlewares/auth.middleware.js";
+import { homeGetController } from "../controllers/home.controllers.js";
 
 const router = Router();
+
+router.get("/", ensureAuth, homeGetController);
 
 export { router };
