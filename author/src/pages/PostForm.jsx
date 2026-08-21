39ba4b76db76912f853bd7ab1531fe05abe97c73
Nivedh E R <nivedher12@gmail.com>
2026-08-21 19:58:14 +0530

    chore(imports): removed unused imports

--- author/src/pages/PostForm.jsx
+++ author/src/pages/PostForm.jsx
@@ -12,7 +12,7 @@ import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useAuth } from "@/hooks/useAuth";
 import { useState } from "react";
-import { useNavigate, Navigate } from "react-router";
+import { useNavigate } from "react-router";
 
 const PostForm = () => {
   const { user, logout } = useAuth();
