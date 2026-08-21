67b2668b2062db1d2785de22be74bc5bd43f4fde
Nivedh E R <nivedher12@gmail.com>
2026-08-21 09:28:03 +0530

    feat(dynamic nav): added dynamic login navigation when not logged in

--- author/src/pages/Home.jsx
+++ author/src/pages/Home.jsx
@@ -1,3 +1,11 @@
-const Home = () => {};
+import { useAuth } from "@/hooks/useAuth";
+import { Navigate } from "react-router";
+
+const Home = () => {
+  const { user } = useAuth();
+  if (!user) {
+    return <Navigate to="/auth/login" replace />;
+  }
+};
 
 export default Home;
