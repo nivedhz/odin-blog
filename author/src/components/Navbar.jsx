156821ea09646b071b50e0b10cedb06fafe5b8ba
Nivedh E R <nivedher12@gmail.com>
2026-08-21 18:41:02 +0530

    feat(dropdown profile): Implemented a dropdown view for profile options

--- author/src/components/Navbar.jsx
+++ author/src/components/Navbar.jsx
@@ -13,6 +13,15 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const Navbar = () => {
   const { user, logout } = useAuth();
@@ -24,47 +33,64 @@ const Navbar = () => {
         </Link>
         {user ? (
           <div className="flex gap-4 items-center">
-            <div className="flex gap-2">
-              <User size={20} />
-              <p>{user.username}</p>
-            </div>
-            <AlertDialog>
-              <AlertDialogTrigger
+            <DropdownMenu>
+              <DropdownMenuTrigger
                 render={
                   <Button
                     className="flex gap-2 cursor-pointer"
-                    title="Logout"
-                    variant="destructive"
+                    variant="outline"
                   >
-                    Logout
-                    <LogOut size={20} />
+                    <User size={20} />
+                    <p>{user.username}</p>
                   </Button>
                 }
-              />
-              <AlertDialogContent className={"dark"}>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>
-                    Are you sure you want to logout?
-                  </AlertDialogTitle>
-                  <AlertDialogDescription>
-                    By logging out you won't be able to access or manage your
-                    blog posts again without logging in
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel className={"cursor-pointer"}>
-                    Cancel
-                  </AlertDialogCancel>
-                  <AlertDialogAction
-                    className={"cursor-pointer"}
-                    onClick={logout}
-                    variant="destructive"
-                  >
-                    Logout
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
+              ></DropdownMenuTrigger>
+              <DropdownMenuContent className={"dark"}>
+                <DropdownMenuGroup>
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuItem>Posts</DropdownMenuItem>
+                  <DropdownMenuItem>Comments</DropdownMenuItem>
+                  <DropdownMenuSeparator></DropdownMenuSeparator>
+                  <AlertDialog>
+                    <AlertDialogTrigger
+                      render={
+                        <Button
+                          className="flex gap-2 cursor-pointer min-w-full items-center justify-start p-2"
+                          title="Logout"
+                          variant="ghost"
+                        >
+                          Logout
+                          <LogOut size={20} />
+                        </Button>
+                      }
+                    />
+                    <AlertDialogContent className={"dark"}>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you sure you want to logout?
+                        </AlertDialogTitle>
+                        <AlertDialogDescription>
+                          By logging out you won't be able to access or manage
+                          your blog posts again without logging in
+                        </AlertDialogDescription>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel className={"cursor-pointer"}>
+                          Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction
+                          className={"cursor-pointer"}
+                          onClick={logout}
+                          variant="destructive"
+                        >
+                          Logout
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
         ) : (
           <div className="flex items-center gap-2">
