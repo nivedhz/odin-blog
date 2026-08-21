3ab30c3dcde4a5add1593ecc9823bf2373dac041
Nivedh E R <nivedher12@gmail.com>
2026-08-21 20:24:13 +0530

    feat(Home): added a basic posts fetching effect to home

--- author/src/pages/Home.jsx
+++ author/src/pages/Home.jsx
@@ -1,16 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
 import { Plus } from "lucide-react";
+import { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router";
 
 const Home = () => {
   const { user } = useAuth();
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user?.token}`,
+        },
+      });
+
+      const result = await response.json();
+      console.log(result);
+      setData(result);
+    };
+    fetchPosts();
+  }, [user?.token]);
   if (!user) {
     return <Navigate to="/auth/login" replace />;
   }
+
   return (
-    <div className="min-h-full min-w-screen p-4">
+    <div className="min-h-full p-4">
       <div className="flex gap-2 items-center justify-end">
         <Button
           className={"font-bold p-4 cursor-pointer flex items-center"}
@@ -18,9 +36,23 @@ const Home = () => {
             navigate("/post/new");
           }}
         >
-          <Plus></Plus>
+          <Plus />
           New Post
         </Button>
+      </div>
+      <div className="">
+        {data.map((item) => {
+          return (
+            <div className="" key={item.id}>
+              <h1>{item.title}</h1>
+              <p>{item.content}</p>
+              <p>Author: {item.author.username}</p>
+              <p>{new Date(item.createdAt).toLocaleString()}</p>
+              <p>published: {item.publishStatus.toString()}</p>
+              <br />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
