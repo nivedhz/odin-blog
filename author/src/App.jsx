4d9198141c4858fc59d4bb6ba3de605c940d4891
Nivedh E R <nivedher12@gmail.com>
2026-08-22 22:01:34 +0530

    fix(loading): removed unnecessary loading from APP

--- author/src/App.jsx
+++ author/src/App.jsx
@@ -1,25 +1,14 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
-import { useAuth } from "./hooks/useAuth";
-import LoadingSpinner from "./components/LoadingSpinner";
 
 function App() {
-  const { loading } = useAuth();
   return (
-    <>
-      {loading ? (
-        <div className="min-h-screen flex justify-center items-center dark bg-background">
-          <LoadingSpinner loading={loading} />
-        </div>
-      ) : (
-        <div className="min-h-screen dark bg-background text-foreground">
-          <Navbar />
-          <main>
-            <Outlet />
-          </main>
-        </div>
-      )}
-    </>
+    <div className="min-h-screen dark bg-background text-foreground">
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
