93ab8457b2700a6f1cee5a99367c206e89b99698
Nivedh E R <nivedher12@gmail.com>
2026-08-22 19:09:47 +0530

    feat(Home): implemented a short hero-section for Home

--- author/src/pages/Home.jsx
+++ author/src/pages/Home.jsx
@@ -1,61 +1,74 @@
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/hooks/useAuth";
-import { Plus } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { ArrowBigRight } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router";
 
 const Home = () => {
-  const { user, logout } = useAuth();
+  const { user } = useAuth();
+  const [loaded, setLoaded] = useState(false);
   const navigate = useNavigate();
-  const [data, setData] = useState([]);
   useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await fetch(`${import.meta.env.VITE_SERVER_URL}/posts`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${user?.token}`,
-        },
-      });
-      if (response.status === 401) {
-        logout();
-        return <Navigate to="/auth/login" />;
-      }
+    const animationFrame = requestAnimationFrame(() => {
+      setLoaded(true);
+    });
 
-      const result = await response.json();
-      setData(result);
-    };
-    fetchPosts();
-  }, [user?.token, logout]);
-  if (!user) {
-    return <Navigate to="/auth/login" replace />;
+    return () => cancelAnimationFrame(animationFrame);
+  }, []);
+
+  if (user) {
+    return <Navigate to="/dashboard" />;
   }
-
   return (
-    <div className="min-h-full p-4">
-      <div className="flex gap-2 items-center justify-end">
+    <div className="min-w-full px-8 py-16 flex items-center gap-10 justify-center transition-all duration-500 ease-out">
+      <div className="flex flex-col gap-4 items-center justify-center">
+        <h1
+          className={cn(
+            "text-6xl text-wrap text-muted-foreground max-w-xl text-center transition-all duration-500 ease-out",
+            {
+              "opacity-100 translate-y-0": loaded,
+              "opacity-0 translate-y-10": !loaded,
+            },
+          )}
+        >
+          Focus on Your Words.
+          <span
+            className={cn("text-foreground", {
+              "opacity-100 translate-y-0": loaded,
+              "opacity-0 translate-y-10": !loaded,
+            })}
+          >
+            We Handle the Rest
+          </span>
+        </h1>
+        <p
+          className={cn(
+            "text-muted-foreground max-w-md text-center transition-all duration-500 ease-out delay-100",
+            {
+              "opacity-100 translate-y-0": loaded,
+              "opacity-0 translate-y-10": !loaded,
+            },
+          )}
+        >
+          A distraction-free writing platform designed for creators, thinkers,
+          and developers.
+        </p>
         <Button
-          className={"font-bold p-4 cursor-pointer flex items-center"}
+          size="lg"
+          className={cn(
+            "transition-all duration-500 ease-out delay-150 cursor-pointer",
+            {
+              "opacity-100 translate-y-0": loaded,
+              "opacity-0 translate-y-10": !loaded,
+            },
+          )}
           onClick={() => {
-            navigate("/post/new");
+            navigate("/auth/sign-up");
           }}
         >
-          <Plus />
-          New Post
+          Start writing for free <ArrowBigRight />
         </Button>
-      </div>
-      <div className="">
-        {data.map((item) => {
-          return (
-            <div className="" key={item.id}>
-              <h1>{item.title}</h1>
-              <p>{item.content}</p>
-              <p>Author: {item.author.username}</p>
-              <p>{new Date(item.createdAt).toLocaleString()}</p>
-              <p>published: {item.publishStatus.toString()}</p>
-              <br />
-            </div>
-          );
-        })}
       </div>
     </div>
   );
