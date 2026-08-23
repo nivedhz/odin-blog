5c04d1d9a280671813554c7e271e2ec1ef3db659
Nivedh E R <nivedher12@gmail.com>
2026-08-23 14:31:15 +0530

    fix(particles): removed particles because of flashing on reload

--- author/src/providers/AuthProvider.jsx
+++ author/src/providers/AuthProvider.jsx
@@ -3,11 +3,12 @@ import { AuthContext } from "../context/AuthContext";
 import LoadingSpinner from "@/components/LoadingSpinner";
 
 export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
   const [user, setUser] = useState(() => {
     const savedUser = localStorage.getItem("user");
+    setLoading(false);
     return savedUser ? JSON.parse(savedUser) : null;
   });
-  const [loading, setLoading] = useState(false);
 
   const login = (userData) => {
     localStorage.setItem("user", JSON.stringify(userData));
