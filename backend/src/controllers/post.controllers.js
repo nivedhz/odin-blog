e3795e61d7dc5c3aea7187e321ada3f8884f7dd7
Nivedh E R <nivedher12@gmail.com>
2026-08-24 14:05:08 +0530

    feat(deletePosts): implemented the routes and controllers for post deletion

--- backend/src/controllers/post.controllers.js
+++ backend/src/controllers/post.controllers.js
@@ -30,3 +30,55 @@ export const newPostPostController = async (req, res, next) => {
     message: "Successfully created the post",
   });
 };
+
+export const postDeleteController = async (req, res, next) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: req.params.postId,
+    },
+  });
+  const user = await prisma.user.findUnique({
+    where: { id: req.user.id },
+    select: {
+      id: true,
+      posts: {
+        include: {
+          author: {
+            select: { username: true },
+          },
+        },
+      },
+    },
+  });
+
+  if (user.id !== post.authorId) {
+    return res.status(401).json({
+      success: false,
+      message: "Cannot delete other user's posts",
+    });
+  }
+  await prisma.post.delete({
+    where: {
+      id: req.params.postId,
+      authorId: req.user.id,
+    },
+  });
+  const userPosts = await prisma.user.findUnique({
+    where: { id: req.user.id },
+    select: {
+      posts: {
+        include: {
+          author: {
+            select: { username: true },
+          },
+        },
+      },
+    },
+  });
+
+  res.json({
+    success: true,
+    message: "Successfully deleted the post",
+    posts: userPosts.posts,
+  });
+};
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {
   newPostPostController,
+  postDeleteController,
   postsGetController,
 } from "../controllers/post.controllers.js";
 import { ensureAuth } from "../middlewares/auth.middleware.js";
@@ -10,5 +11,7 @@ const router = Router();
 router.get("/", ensureAuth, postsGetController);
 
 router.post("/new", ensureAuth, newPostPostController);
+
+router.delete("/delete/:postId", ensureAuth, postDeleteController);
 
 export { router };
