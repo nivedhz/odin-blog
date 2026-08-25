25d76abcdfcf9f68ca265c1ab898a8ee435aa305
Nivedh E R <nivedher12@gmail.com>
2026-08-25 19:57:46 +0530

    fix(kinda fix scrollTo): added redirect to dashboard if not in dashboard

--- frontend/author/src/providers/ScrollProvider.jsx
+++ frontend/author/src/providers/ScrollProvider.jsx
@@ -1,12 +1,18 @@
 import { ScrollContext } from "@/context/ScrollContext";
+import { useNavigate, useLocation } from "react-router";
 import { useRef } from "react";
 
 export function ScrollProvider({ children }) {
   const draftRef = useRef(null);
   const postRef = useRef(null);
   const publishRef = useRef(null);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const scrollTo = (ref) => {
+    if (location.pathname !== "/dashboard") {
+      return navigate("/dashboard");
+    }
     ref.current?.scrollIntoView({ behavior: "smooth" });
   };
 
