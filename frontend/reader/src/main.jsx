7705f290c805137fb5d25da15db4e86be6989d15
Nivedh E R <nivedher12@gmail.com>
2026-08-27 11:38:51 +0530

    feat(RouterProvider): added router provider to deal with routers

--- frontend/reader/src/main.jsx
+++ frontend/reader/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router";
+import { AuthProvider } from "./providers/AuthProvider.jsx";
+import router from "./routes/index.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
-)
+);
