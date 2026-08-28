fb976e4a3a33429a62aa66f0e8d10ce35e6af875
Nivedh E R <nivedher12@gmail.com>
2026-08-28 20:06:24 +0530

    fix(Error handling): changed the status code and message

--- backend/src/controllers/reader/reader.controllers.js
+++ backend/src/controllers/reader/reader.controllers.js
@@ -3,9 +3,9 @@ import { prisma } from "../../lib/prisma.js";
 export const postGetController = async (req, res, next) => {
   const posts = await prisma.post.findMany();
   if (!posts) {
-    return res.status(500).json({
+    return res.status(404).json({
       success: false,
-      message: "Internal server error",
+      message: "No posts found",
     });
   }
   res.status(200).json(posts);
