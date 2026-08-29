4d894c4bfe2c42fe13dcdb1e9c75d59d6e048525
Nivedh E R <nivedher12@gmail.com>
2026-08-29 20:33:17 +0530

    feat(post and comments): added a get all published posts, get one post and get all comments for a post controllers

--- backend/src/controllers/reader/reader.controllers.js
+++ backend/src/controllers/reader/reader.controllers.js
@@ -1,7 +1,18 @@
 import { prisma } from "../../lib/prisma.js";
 
-export const postGetController = async (req, res, next) => {
-  const posts = await prisma.post.findMany();
+export const postsGetController = async (req, res, next) => {
+  const posts = await prisma.post.findMany({
+    where: {
+      publishStatus: true,
+    },
+    include: {
+      author: {
+        select: {
+          username: true,
+        },
+      },
+    },
+  });
   if (!posts) {
     return res.status(404).json({
       success: false,
@@ -9,4 +20,64 @@ export const postGetController = async (req, res, next) => {
     });
   }
   res.status(200).json(posts);
+};
+
+export const postGetController = async (req, res, next) => {
+  const post = await prisma.post.findUnique({
+    where: {
+      id: req.params.postId,
+    },
+    include: {
+      author: {
+        select: {
+          username: true,
+        },
+      },
+      comments: {
+        include: {
+          creator: {
+            select: {
+              username: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!post) {
+    return res.status(404).json({
+      success: false,
+      message: "Post not found",
+    });
+  }
+
+  res.json({
+    success: true,
+    post: post,
+  });
+};
+
+export const commentPutController = async (req, res, next) => {
+  await prisma.comment.create({
+    data: {
+      content: req.body.comment,
+      postId: req.body.post,
+      creatorId: req.user.id,
+    },
+  });
+  const comments = await prisma.comment.findMany({
+    where: {
+      postId: req.body.post,
+    },
+    include: {
+      creator: true,
+    },
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Created comment successfully",
+    comments,
+  });
 };
