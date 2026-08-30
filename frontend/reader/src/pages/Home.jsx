8370b1f6281b2eafd219fb3b61d5dcb7ea1d40ad
Nivedh E R <nivedher12@gmail.com>
2026-08-30 18:48:02 +0530

    feat(Sort section): added the sort recent section to the homepae

--- frontend/reader/src/pages/Home.jsx
+++ frontend/reader/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import LoadingSpinner from "#components/LoadingSpinner";
 
 const Home = () => {
   const [data, setData] = useState(null);
+  const [recentPosts, setRecentPosts] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [loaded, setLoaded] = useState(false);
@@ -39,6 +40,28 @@ const Home = () => {
     };
     fetchPosts();
   }, []);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_SERVER_URL}/reader/post?sort=recent`,
+          {
+            method: "GET",
+          },
+        );
+        const result = await response.json();
+        setRecentPosts(result);
+        setError(null);
+      } catch (err) {
+        console.error(err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchPosts();
+  }, []);
 
   if (loading) {
     return (
@@ -48,35 +71,70 @@ const Home = () => {
     );
   }
   return (
-    <div className="px-25 py-10 flex flex-col gap-4">
-      <div
-        className={cn("transition-all duration-500 ease-out", {
-          "opacity-100 translate-y-0": loaded,
-          "opacity-0 translate-y-10": !loaded,
-        })}
-      >
-        <h1 className="text-4xl font-bold">Popular Posts</h1>
+    <>
+      <div className="px-25 py-10 flex flex-col gap-4">
+        <div
+          className={cn("transition-all duration-500 ease-out", {
+            "opacity-100 translate-y-0": loaded,
+            "opacity-0 translate-y-10": !loaded,
+          })}
+        >
+          <h1 className="text-4xl font-bold">Popular Posts</h1>
+        </div>
+        <div
+          className={cn("px-4 flex flex-col gap-4", {
+            "opacity-100 translate-y-0": loaded,
+            "opacity-0 translate-y-10": !loaded,
+          })}
+        >
+          {data?.map((item, index) => (
+            <div
+              key={item.id}
+              style={{ transitionDelay: `${index * 50 + 50}ms` }}
+              className={cn(
+                "transition-all duration-500 ease-out",
+                loaded
+                  ? "opacity-100 translate-y-0"
+                  : "opacity-0 translate-y-10",
+              )}
+            >
+              <Post item={item} />
+            </div>
+          ))}
+        </div>
       </div>
-      <div
-        className={cn("px-4 flex flex-col gap-4", {
-          "opacity-100 translate-y-0": loaded,
-          "opacity-0 translate-y-10": !loaded,
-        })}
-      >
-        {data?.map((item, index) => (
-          <div
-            key={item.id}
-            style={{ transitionDelay: `${index * 50 + 50}ms` }}
-            className={cn(
-              "transition-all duration-500 ease-out",
-              loaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10",
-            )}
-          >
-            <Post item={item} />
-          </div>
-        ))}
+      <div className="px-25 py-10 flex flex-col gap-4">
+        <div
+          className={cn("transition-all duration-500 ease-out delay-100", {
+            "opacity-100 translate-y-0": loaded,
+            "opacity-0 translate-y-10": !loaded,
+          })}
+        >
+          <h1 className="text-4xl font-bold">Recent Posts</h1>
+        </div>
+        <div
+          className={cn("px-4 flex flex-col gap-4", {
+            "opacity-100 translate-y-0": loaded,
+            "opacity-0 translate-y-10": !loaded,
+          })}
+        >
+          {recentPosts?.map((item, index) => (
+            <div
+              key={item.id}
+              style={{ transitionDelay: `${index * 50 + 150}ms` }}
+              className={cn(
+                "transition-all duration-500 ease-out",
+                loaded
+                  ? "opacity-100 translate-y-0"
+                  : "opacity-0 translate-y-10",
+              )}
+            >
+              <Post item={item} />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
